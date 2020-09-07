b9e20a1291341e3a810a335e03ec4bf4ccd38b83
Danilo Alves <danilo.alves@kinvo.com.br>
2020-09-06 22:28:25 -0300

    feat(App): Adds some mocked movies

--- src/App/presentational.tsx
+++ src/App/presentational.tsx
@@ -1,13 +1,65 @@
 // Dependencies
 import React from 'react';
 
+// Components
+import { Movie } from '../components';
+
 // Styled components
 import { Container } from './styles';
 
 function App() {
   return (
     <Container>
-      <div />
+      <Movie
+        id="1"
+        title="Pitch Perfect"
+        synopsis="Pitch Perfect é uma série de filmes de comédia musical americana criada por Kay Cannon, vagamente baseada no livro de não ficção Pitch Perfect: The Quest for Collegiate a Cappella Glory de Mickey Rapkin. Jason Moore dirigiu o primeiro filme, e Elizabeth Banks dirigiu o segundo e Trish Sie dirigiu o terceiro."
+        releaseDate="2020-07-22"
+        categories={[
+          'Comédia',
+          'Musical',
+          'Teste 1',
+          'Teste 2',
+          'Teste 3',
+          'teste 4',
+        ]}
+        hasBeenWatched={false}
+        isBeingWatched={false}
+      />
+
+      <Movie
+        id="2"
+        title="Pitch Perfect"
+        synopsis="Pitch Perfect é uma série de filmes de comédia musical americana criada por Kay Cannon, vagamente baseada no livro de não ficção Pitch Perfect: The Quest for Collegiate a Cappella Glory de Mickey Rapkin. Jason Moore dirigiu o primeiro filme, e Elizabeth Banks dirigiu o segundo e Trish Sie dirigiu o terceiro."
+        releaseDate="2020-07-22"
+        categories={[
+          'Comédia',
+          'Musical',
+          'Teste 1',
+          'Teste 2',
+          'Teste 3',
+          'teste 4',
+        ]}
+        hasBeenWatched={true}
+        isBeingWatched={false}
+      />
+
+      <Movie
+        id="3"
+        title="Pitch Perfect"
+        synopsis="Pitch Perfect é uma série de filmes de comédia musical americana criada por Kay Cannon, vagamente baseada no livro de não ficção Pitch Perfect: The Quest for Collegiate a Cappella Glory de Mickey Rapkin. Jason Moore dirigiu o primeiro filme, e Elizabeth Banks dirigiu o segundo e Trish Sie dirigiu o terceiro."
+        releaseDate="2020-07-22"
+        categories={[
+          'Comédia',
+          'Musical',
+          'Teste 1',
+          'Teste 2',
+          'Teste 3',
+          'teste 4',
+        ]}
+        hasBeenWatched={true}
+        isBeingWatched={true}
+      />
     </Container>
   );
 }
--- src/App/styles.ts
+++ src/App/styles.ts
@@ -1,4 +1,13 @@
 // Dependencies
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 400px);
+  grid-gap: 10px;
+  align-content: center;
+  justify-content: center;
+  min-height: 100vh;
+
+  background-color: #ecf0f2;
+`;
