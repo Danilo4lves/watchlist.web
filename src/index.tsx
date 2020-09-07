a7e17d1ef867027788a23d78b23f9efba2b6868c
Danilo Alves <danilo.alves@kinvo.com.br>
2020-09-06 22:04:27 -0300

    feat: Adds global style to app root

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 // Dependencies
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Styles utitilities
+import { GlobalStyle } from './styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
